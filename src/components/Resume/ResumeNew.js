8d08af1101fb8eae34d45d6a947ae925a714648d
Chirag Simkhada <chiragsimkhada@gmail.com>
2022-10-05 08:40:27 +0545

    CV added

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,7 +9,7 @@ import Particle from "../Particle";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/soumyajit4419/portfolio/master/src/Assets/Soumyajit_Behera-BIT_MESRA.pdf";
+  "https://raw.githubusercontent.com/Chirag123-bit/Portfolio/master/src/Assets/ChiragSimkhada.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
